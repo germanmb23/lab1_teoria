b865ff33a887c33704c75190e5c23b6a2f3eb653
German M <german.moreira@fing.edu.uy>
2020-03-25 18:00:58 -0300

    programa2

--- programas/programa2.py
+++ programas/programa2.py
@@ -2,9 +2,22 @@
 import re
 import sys
 
+from programa1 import programa as limpiar
+
 def programa(texto):
     # Implementar programa
-
+    texto = limpiar(texto)
+    si = re.findall(r"\s+si\s", texto, re.DOTALL)
+    para = re.findall(r"\s+para\s[^c][^a][^d][^a]", texto, re.DOTALL)
+    paracada = re.findall(r"\s+para\scada\s", texto, re.DOTALL)
+    mientras = re.findall(r"\s+mientras\s", texto, re.DOTALL)
+    
+    res = ""
+    res += 'si: ' + str(len(si))
+    res += "\npara: " + str(len(para))
+    res += "\npara-cada: " + str(len(paracada))
+    res += "\nmientras: " + str(len(mientras))
+    return res 
 
 if __name__ == '__main__':
     archivo_entrada = sys.argv[1]
