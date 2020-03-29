dfc98ca497b08aee5be75cff5244d31fee4c2906
javier <gayoso.javier@gmail.com>
2020-03-29 20:29:26 -0300

    Programa 4

--- programas/programa4.py
+++ programas/programa4.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
-
+from programa1 import programa as limpiar
 def programa(texto):
     # Implementar programa
-    return texto
+    texto = limpiar(texto)
+    #op_booleanas = re.findall(r"(\s[><]\s|\s(&&)\s|\s(>=)\s|\s(<=)\s|\s(==)\s|\s(=&&)\s)", texto, flags = 0)
+    op_booleanas = re.findall(r"([^<>&=\-\(]|\s|[a-z, , A-Z, 0-9,\_])([><]|(&&)|(>=)|(<=)|(==)|(=&&))([^<>&=\-\), A-Z]|\s|[a-z, 0-9, \_])", texto, flags = 0)
+    #op = re.findall(r"\s[\/\*\-+]\s", texto, flags = 0)     
+    op = re.findall(r"[^\/\*\-+<>][\/\*\-+][^\/\*\-+<>]", texto, flags = 0)  
+    
+    res = ""
+    res += "booleanas: " + str(len(op_booleanas))
+    res += "\nno booleanas: " + str(len(op))
+    
+    return res 
 
 if __name__ == '__main__':
     archivo_entrada = sys.argv[1]
