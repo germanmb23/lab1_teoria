b4d858a1397ed24deda44cf3a0c3706e27ae722d
javier <gayoso.javier@gmail.com>
2020-04-01 17:25:00 -0300

    Programa 5, todos los test OK!!

--- programas/programa3.py
+++ programas/programa3.py
@@ -3,10 +3,11 @@ import re
 import sys
 
 def programa(texto):
-    nombre = re.findall(r"\s*funcion\s+(\w*)", texto)
-    parametros = re.findall(r"\s*funcion.*?\((.*?)\)", texto, re.S)
-    tipo_salida = re.findall(r"\s*funcion\s+.*?->\s*(.*?)\s*\{", texto)
-    res = f"{nombre[0]}"
+    nombre = re.findall(r"funcion\s+(\w*)", texto)
+    res = ""
+    parametros = re.findall(r"funcion.*?\((.+?)\)", texto, re.S)
+    tipo_salida = re.findall(r"funcion\s+.*?->\s*(.+?)\s*\{", texto)
+    res += nombre[0]
     if parametros : res += f"\n{parametros[0]}"
     if tipo_salida : res += f"\n{tipo_salida[0]}"
     return res
